e2ed3db62892afd1aaf410e41b17974abc6c47f8
mohmoh10999 <mohmoh10999@gmail.com>
2021-06-02 13:41:09 +0630

    practice the debug

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show , :destroy]
   layout "portfolio"
   def index
     @portfolio_items = Portfolio.all
@@ -20,15 +21,12 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
@@ -40,9 +38,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -60,6 +55,10 @@ class PortfoliosController < ApplicationController
                                       :body,
                                       technologies_attributes: [:name]
                                      )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,13 @@ class Blog < ApplicationRecord
   enum status: {draft: 0 ,published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def self.special_blogs
+  	all
+  end
+
+  def self.featured_blogs
+  	limit(2)
+  end
+
 end
